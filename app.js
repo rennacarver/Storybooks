ee77add8f02af3e68f1d615516ce162a1b759749
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2024-03-04 23:40:41 -0800

    add Google Auth and passport.js

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const connectDB = require('./config/db')
+
 
 //load config
 dotenv.config({ path: './config/config.env'})
+
+// Passport config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -16,6 +22,17 @@ const app = express()
 app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
+// Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}))
+
+// Passport
+app.use(passport.initialize())
+app.use(passport.session())
+
 //STATIC
 app.use(express.static('public'))
 
@@ -25,6 +42,7 @@ if(process.env.NODE_ENV === 'development')
 
 //ROUTES
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 5000
 
